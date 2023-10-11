279242b33e0ced2238d1dc657b85e54c7f418736
Samuel Gómez <sagomadev@gmail.com>
2023-10-11 20:27:44 +0200

    Add counter state

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,7 +1,31 @@
+"use client";
+
+import { useState } from "react";
+
 export default function CounterPage() {
+  const [count, setCount] = useState(0);
+
+  const handlerClickButton = (value: number) => {
+    setCount(count + value);
+  };
+
   return (
-    <div>
-      <h1>Hello Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span className="text-9xl">{count}</span>
+      <div className="flex">
+        <button
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+          onClick={() => handlerClickButton(1)}
+        >
+          +1
+        </button>
+        <button
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+          onClick={() => handlerClickButton(-1)}
+        >
+          -1
+        </button>
+      </div>
     </div>
   );
 }
