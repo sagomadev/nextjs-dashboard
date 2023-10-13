97e8f9381f5388b74183765d5882fac62794ad49
Samuel Gómez <sagomadev@gmail.com>
2023-10-13 16:16:38 +0200

    list the first 151 pokemons

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,11 +1,38 @@
-const getPokemons = async (limit: number = 20, offset: number = 0) => {
-  const data = await fetch(
+import { PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+import Image from "next/image";
+
+const getPokemons = async (
+  limit: number = 20,
+  offset: number = 0
+): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await fetch(
     `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
   ).then((res) => res.json());
-  return data;
+
+  const pokemons = data.results.map((pokemon) => ({
+    id: pokemon.url.split("/").at(-2)!,
+    name: pokemon.name,
+  }));
+
+  return pokemons;
 };
+
 export default async function PokemonsPage() {
   const pokemons = await getPokemons(151);
 
-  return <div>{JSON.stringify(pokemons)}</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-wrap gap-10 items-center justify-start">
+        {pokemons.map((pokemon) => (
+          <Image
+            key={pokemon.id}
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
+            alt={pokemon.name}
+            width={100}
+            height={100}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
