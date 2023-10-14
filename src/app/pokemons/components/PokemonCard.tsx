1f8cfd7cab399c22f735a1a30ac3fdd7cd0bf0bc
Samuel Gómez <sagomadev@gmail.com>
2023-10-14 02:41:02 +0200

    add image property to lazzy load

--- src/app/pokemons/components/PokemonCard.tsx
+++ src/app/pokemons/components/PokemonCard.tsx
@@ -19,6 +19,7 @@ export const PokemonCard = ({ pokemon }: Props) => {
             alt={pokemon.name}
             width={100}
             height={100}
+            priority={false}
           />
           <p className="pt-2 text-lg font-semibold text-gray-50 capitalize">
             {name}
