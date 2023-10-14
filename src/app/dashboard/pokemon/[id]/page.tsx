d528cfd1411242bf3348feb3148ad4f19d113b31
Samuel Gómez <sagomadev@gmail.com>
2023-10-14 17:54:31 +0200

    Add dynamic metadata

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,7 +1,16 @@
 import { Pokemon } from "@/pokemons";
+import { Metadata } from "next";
 
 interface Props {
   params: { id: string };
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id, name } = await getPokemon(params.id);
+  return {
+    title: `Pokemon ${name} Nº ${id}`,
+    description: `Página del pokémon ${name} Nº ${id}`,
+  };
 }
 
 const getPokemon = async (id: string): Promise<Pokemon> => {
@@ -17,7 +26,7 @@ export default async function PokemonPage({ params }: Props) {
   return (
     <div>
       <h1>Hello PokemonPage</h1>
-      <div>{JSON.stringify(pokemon)}</div>
+      <div>{pokemon.name}</div>
     </div>
   );
 }
