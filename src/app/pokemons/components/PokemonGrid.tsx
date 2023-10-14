1b3568690dcf8f37b13f73f694c569ee16e98f18
Samuel Gómez <sagomadev@gmail.com>
2023-10-14 02:20:39 +0200

    add PokemonCard Component

--- src/app/pokemons/components/PokemonGrid.tsx
+++ src/app/pokemons/components/PokemonGrid.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { SimplePokemon } from "../";
+import { PokemonCard } from "./PokemonCard";
 
 interface Props {
   pokemons: SimplePokemon[];
@@ -9,13 +10,7 @@ export const PokemonGrid = ({ pokemons }: Props) => {
   return (
     <div className="flex flex-wrap gap-10 items-center justify-start">
       {pokemons.map((pokemon) => (
-        <Image
-          key={pokemon.id}
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
-          alt={pokemon.name}
-          width={100}
-          height={100}
-        />
+        <PokemonCard key={pokemon.id} />
       ))}
     </div>
   );
