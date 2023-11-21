57eaed21b8b11809048577f71fc07b5efca87636
Samuel Gómez <sagomadev@gmail.com>
2023-11-21 08:57:04 +0100

    Add configuration to use typescript with reducers

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import counterReducer from "./counter/counterSlice";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 export const store = configureStore({
   reducer: {
@@ -12,3 +13,7 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
