c19bb678fbd7d26e08d5e16722fa62e416292b83
Samuel Gómez <sagomadev@gmail.com>
2023-11-23 19:54:09 +0100

    Pass params to widget

--- src/components/dashboard/SimpleWidget.tsx
+++ src/components/dashboard/SimpleWidget.tsx
@@ -1,11 +1,19 @@
 import { IoCafeOutline } from "react-icons/io5";
 
-export const SimpleWidget = () => {
+interface Props {
+  title: string;
+  subtitle?: string;
+  label?: string;
+  icon?: React.ReactNode;
+  href?: string;
+}
+
+export const SimpleWidget = ({ title, subtitle, label }: Props) => {
   return (
     <div className="bg-white shadow-xl p-3 sm:min-w-[25%] min-w-full  rounded-2xl border-1 border-gray-50 mx-2">
       <div className="flex flex-col">
         <div>
-          <h2 className="font-bold text-gray-600 text-center">Contador</h2>
+          <h2 className="font-bold text-gray-600 text-center">{label}</h2>
         </div>
         <div className="my-3">
           <div className="flex flex-row items-center justify-center space-x-1 ">
--- src/components/dashboard/WidgetsGrid.tsx
+++ src/components/dashboard/WidgetsGrid.tsx
@@ -1,8 +1,14 @@
+import { IoCarOutline } from "react-icons/io5";
 import { SimpleWidget } from "./SimpleWidget";
 export const WidgetsGrid = () => {
   return (
     <div className="flex flex-wrap p-2">
-      <SimpleWidget />
+      <SimpleWidget
+        title="Contador"
+        subtitle="Productos agragados"
+        label="Contador"
+        icon={<IoCarOutline size={70} className="text-blue-600" />}
+      />
     </div>
   );
 };
