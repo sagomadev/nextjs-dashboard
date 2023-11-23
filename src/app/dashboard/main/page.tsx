50b368f16f0ed911211344056b49f56f6b8ef985
Samuel Gómez <sagomadev@gmail.com>
2023-11-23 19:45:45 +0100

    Add WidgetsGrid

--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,13 +1,11 @@
-import { SimpleWidget } from "@/components";
+import { WidgetsGrid } from "@/components";
 
 export default function MainPage() {
   return (
     <div className="text-black p-2">
       <h1 className="mt-2 text-3xl">Dashboard</h1>
       <span className="text-xl">Información general</span>
-      <div className="flex flex-wrap p-2">
-        <SimpleWidget />
-      </div>
+      <WidgetsGrid />
     </div>
   );
 }
---
+++ src/components/dashboard/WidgetsGrid.tsx
@@ -0,0 +1,8 @@
+import { SimpleWidget } from "./SimpleWidget";
+export const WidgetsGrid = () => {
+  return (
+    <div className="flex flex-wrap p-2">
+      <SimpleWidget />
+    </div>
+  );
+};
