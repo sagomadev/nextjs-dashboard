1e0296857d4ae056666a0515d6c80905deebdd0d
Samuel Gómez <sagomadev@gmail.com>
2023-12-06 12:53:42 +0100

    Add use client directive and pass props

--- src/components/dashboard/WidgetsGrid.tsx
+++ src/components/dashboard/WidgetsGrid.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import { IoCarOutline } from "react-icons/io5";
 import { SimpleWidget } from "./SimpleWidget";
+import { useAppSelector } from "@/store";
+
 export const WidgetsGrid = () => {
+  const isCart = useAppSelector((state) => state.counter.count);
   return (
     <div className="flex flex-wrap p-2">
       <SimpleWidget
-        title="Contador"
+        title={`${isCart}`}
         subtitle="Productos agragados"
         label="Contador"
         icon={<IoCarOutline size={70} className="text-blue-600" />}
