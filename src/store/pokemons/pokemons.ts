ff3854cbd1216232b2f9a3a9cd052c0e8a2b0831
Samuel Gómez <sagomadev@gmail.com>
2023-12-08 18:37:12 +0100

    add reducer fav toggle

--- src/store/pokemons/pokemons.ts
+++ src/store/pokemons/pokemons.ts
@@ -1,5 +1,5 @@
 import { SimplePokemon } from "@/pokemons";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface PokemonsState {
   [key: string]: SimplePokemon;
@@ -12,9 +12,18 @@ const initialState = {
 const pokemonsSlice = createSlice({
   name: "pokemons",
   initialState,
-  reducers: {},
+  reducers: {
+    toggleFavorite(state, action: PayloadAction<SimplePokemon>) {
+      const pokemon = action.payload;
+      if (!!state[pokemon.id]) {
+        delete state[pokemon.id];
+        return;
+      }
+      state[pokemon.id] = pokemon;
+    },
+  },
 });
 
-export const {} = pokemonsSlice.actions;
+export const { toggleFavorite } = pokemonsSlice.actions;
 
 export default pokemonsSlice.reducer;
