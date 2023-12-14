0c8f1034cf50a00d19c18dba5ea180f693e09112
Samuel Gómez <sagomadev@gmail.com>
2023-12-14 18:29:36 +0100

    Add empty fav list

--- src/pokemons/components/FavoritePokemons.tsx
+++ src/pokemons/components/FavoritePokemons.tsx
@@ -4,11 +4,30 @@ import { useAppSelector } from "@/store";
 import { PokemonGrid } from "./PokemonGrid";
 import { useState } from "react";
 import { SimplePokemon } from "@/pokemons";
+import { IoHeartOutline } from "react-icons/io5";
 
 export const FavoritePokemons = () => {
   const favoritePokemons = useAppSelector((state) =>
     Object.values(state.pokemons)
   );
   const [pokemons, setPokemons] = useState<SimplePokemon[]>(favoritePokemons);
-  return <PokemonGrid pokemons={pokemons} />;
+
+  return (
+    <>
+      {pokemons.length === 0 ? (
+        <EmptyPokemonGrid />
+      ) : (
+        <PokemonGrid pokemons={pokemons} />
+      )}
+    </>
+  );
+};
+
+const EmptyPokemonGrid = () => {
+  return (
+    <div className="flex flex-col h-[50vh] items-center justify-center">
+      <IoHeartOutline size={100} className="text-red-500" />
+      <span>No hay favoritos</span>
+    </div>
+  );
 };
