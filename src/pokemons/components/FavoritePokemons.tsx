97f6ae5d0a13db511b2263f70c4fea70931638ba
Samuel Gómez <sagomadev@gmail.com>
2023-12-14 14:46:03 +0100

    Persist pokemons in favorite page for deselect

--- src/pokemons/components/FavoritePokemons.tsx
+++ src/pokemons/components/FavoritePokemons.tsx
@@ -2,10 +2,13 @@
 
 import { useAppSelector } from "@/store";
 import { PokemonGrid } from "./PokemonGrid";
+import { useState } from "react";
+import { SimplePokemon } from "@/pokemons";
 
 export const FavoritePokemons = () => {
   const favoritePokemons = useAppSelector((state) =>
     Object.values(state.pokemons)
   );
-  return <PokemonGrid pokemons={favoritePokemons} />;
+  const [pokemons, setPokemons] = useState<SimplePokemon[]>(favoritePokemons);
+  return <PokemonGrid pokemons={pokemons} />;
 };
